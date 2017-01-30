078e64d78b7f90c7b5d9fb2ae5980836b0f39554
Jono Belotti <jbelotti@zendesk.com>
2017-01-30 22:39:27 +1100

    Adding photo to site, and adding check for user existence function

--- web_app.rb
+++ web_app.rb
@@ -4,14 +4,52 @@ require 'sinatra'
 require 'octokit'
 require 'slim'
 
-token = "[TEMPORARY PERSONAL ACCESS TOKEN]"
-org_id = 1227580
+require './creds.rb'
 
+token = Credentials.access_token
+org_id = 1227580
 client = Octokit::Client.new(access_token: token)
 
-get "/" do
-  slim :index
+def check_user_exists(client, user)
+  begin
+  profile = client.user(user)
+  rescue Octokit::NotFound
+    return false
+  end
+  return true
 end
+
+@layout =<<EOS
+
+doctype html
+html
+  head
+    title Registration
+    link href="/css/bootstrap.css" rel="stylesheet" type="text/css"
+    link href="/css/bootstrap-responsive.css" rel="stylesheet" type="text/css"
+  body
+    img{src==avatar height='100px' width='100px'}
+    h1 GitHub
+    form action="add" method="POST"
+      p Please enter your GitHub username
+      p
+        input name="github"
+      p
+        input type="submit" value="Add me to organization"
+EOS
+
+@post_text = "post text"
+avatar = "https://assets-cdn.github.com/images/modules/logos_page/Octocat.png"
+
+l = Slim::Template.new { @layout }
+
+get "/" do
+  slim l.render(Object.new, :avatar => avatar)
+end
+
+#get "/" do
+  #slim :index
+#end
 
 post "/add" do
   client.add_team_membership(org_id, params["github"])
