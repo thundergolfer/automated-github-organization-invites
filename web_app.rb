16986582805c952098642d68cdc43bfa59d5eda5
Jono Belotti <jbelotti@zendesk.com>
2017-02-01 23:02:41 +1100

    fixing success message bug

--- web_app.rb
+++ web_app.rb
@@ -86,6 +86,7 @@ post "/add" do
   if user_exists?(client, params["github"])
     client.update_organization_membership(org_name, :user => params["github"])
     "OK, Check your EMAIL"
+  else
+    "User not found. Please check your spelling"
   end
-  "User not found. Please check your spelling"
 end
