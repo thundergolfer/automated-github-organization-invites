cbe459f707c69c93ca0aee16d465f029e3a1ef19
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2021-04-10 14:40:36 +1000

    Stop 4xx errors from Github crashing the server. Instead present the 4xx error message to the user

--- web_app.rb
+++ web_app.rb
@@ -4,20 +4,20 @@ require 'sinatra'
 require 'slim'
 require 'octokit'
 
-token = ENV['GITHUB_TOKEN']
-org_name = ENV['ORGANIZATION_NAME']
-team_name = ENV['TEAM_NAME']
-background_choice = ENV['BACKGROUND_COLOR']
+TOKEN = ENV['GITHUB_TOKEN']
+ORG_NAME = ENV['ORGANIZATION_NAME']
+TEAM_NAME = ENV['TEAM_NAME']
+BACKGROUND_CHOICE = ENV['BACKGROUND_COLOR']
 
-if background_choice == 'green'
+if BACKGROUND_CHOICE == 'green'
     background_css = "/css/background_colors/green.css"
-elsif background_choice == 'blue'
+elsif BACKGROUND_CHOICE == 'blue'
     background_css = "/css/background_colors/blue.css"
-elsif background_choice == 'pink'
+elsif BACKGROUND_CHOICE == 'pink'
     background_css = "/css/background_colors/pink.css"
-elsif background_choice == 'red'
+elsif BACKGROUND_CHOICE == 'red'
     background_css = "/css/background_colors/red.css"
-elsif background_choice == 'grey'
+elsif BACKGROUND_CHOICE == 'grey'
     background_css = "/css/background_colors/grey.css"
 else
     background_css = "/css/background_colors/white.css"
@@ -26,7 +26,7 @@ end
 html_template_path = File.join(__dir__, 'views', 'index.slim')
 @layout = File.read(html_template_path)
 
-client = Octokit::Client.new(access_token: token)
+client = Octokit::Client.new(access_token: TOKEN)
 
 def user_exists?(client, user)
   begin
@@ -37,71 +37,88 @@ def user_exists?(client, user)
   return true
 end
 
-def get_org_avatar_url(client, org_name)
+def get_org_avatar_url(client)
   begin
-    org = client.user(org_name)
+    org = client.user(ORG_NAME)
   rescue Octokit::NotFound
     return nil
   end
   org[:avatar_url]
 end
 
-def get_org_id(client, org_name)
+def get_org_id(client)
   begin
-    org = client.user(org_name)
+    org = client.user(ORG_NAME)
     return org.id
   rescue Octokit::NotFound
     return nil
   end
 end
 
-def get_org_teams(client, org_name)
+# TODO(Jonathon): Remove unused function
+def get_org_teams(client)
   begin
-    teams = client.org_teams(org_name)
+    teams = client.org_teams(ORG_NAME)
     return teams
   rescue Octokit::NotFound
     return nil
   end
 end
 
-def check_org_exists(client, org_name)
-  unless get_org_avatar_url(client, org_name).nil?
+def check_org_exists(client)
+  unless get_org_avatar_url(client).nil?
     return true
   end
   return false
 end
 
-def get_team(client, org_name, team_name)
-  teams = client.org_teams(org_name)
-  team = teams.find {|t| t.slug.downcase == team_name.downcase }    
+def get_team(client)
+  teams = client.org_teams(ORG_NAME)
+  team = teams.find {|t| t.slug.downcase == TEAM_NAME.downcase }
   return team
 end
 
+def add_user_to_org(client, username)
+  begin
+    client.update_organization_membership(ORG_NAME, :user => params["github-user"])
+    "Sent invite to join '" + ORG_NAME + "', Check your EMAIL"
+  rescue Octokit::ClientError => e
+    "Error: #{e.class}. #{e.message}"
+  end
+end
+
+def add_user_to_team_in_org(client, username, team_id)
+  begin
+    client.add_team_membership(team.id, username)
+    "Sent invite to join '" + ORG_NAME + "' and team '" + TEAM_NAME + "', Check your EMAIL"
+  rescue Octokit::ClientError => e
+    "Error: #{e.class}. #{e.message}"
+  end
+end
+
 # The URL for the Organisation's picture/avatar
-avatar = get_org_avatar_url(client, org_name)
-org_id = get_org_id(client, org_name)
+avatar = get_org_avatar_url(client)
+org_id = get_org_id(client)
 
 l = Slim::Template.new { @layout }
 
 # ROUTES #
 
 get "/" do
-  slim l.render(Object.new, :avatar => avatar, :org_name => org_name, :background_css => background_css)
+  slim l.render(Object.new, :avatar => avatar, :org_name => ORG_NAME, :background_css => background_css)
 end
 
 post "/add" do
-  if user_exists?(client, params["github-user"])
-    team = get_team(client, org_name, team_name)    
-    if team.nil?
-      # team was blank or could not be found, just add user to org
-      client.update_organization_membership(org_name, :user => params["github-user"])
-      "Sent invite to join '" + org_name + "', Check your EMAIL"
-    else
-      # team_id valid so invite member directly to org's team
-      client.add_team_membership(team.id, params["github-user"])      
-      "Sent invite to join '" + org_name + "' and team '" + team_name + "', Check your EMAIL"
-    end
+  username = params["github-user"]
+  unless user_exists?(client, username)
+    return "User not found. Please check your spelling"
+  end
+
+  team = get_team(client)
+  if team.nil?
+    # team was blank or could not be found, just add user to org
+    add_user_to_org(client, username)
   else
-    "User not found. Please check your spelling"
+    add_user_to_team_in_org(client, username, team.id)
   end
 end
